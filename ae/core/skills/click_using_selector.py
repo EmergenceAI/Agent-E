1376b7384a367d39ad798100dcd49472aadc8d6d
DeepakAkkil <deepak.akkil@merlyn.org>
2024-06-06 18:11:01 +0300

    Eval improvements temp

--- ae/core/skills/click_using_selector.py
+++ ae/core/skills/click_using_selector.py
@@ -8,7 +8,8 @@ from playwright.async_api import Page
 from ae.core.playwright_manager import PlaywrightManager
 from ae.utils.dom_helper import get_element_outer_html
 from ae.utils.logger import logger
-
+from ae.utils.dom_mutation_observer import subscribe 
+from ae.utils.dom_mutation_observer import unsubscribe 
 
 async def click(selector: Annotated[str, "The properly formed query selector string to identify the element for the click action. When \"mmid\" attribute is present, use it for the query selector."],
                 wait_before_execution: Annotated[float, "Optional wait time in seconds before executing the click event logic.", float] = 0.0) -> Annotated[str, "A message indicating success or failure of the click."]:
@@ -33,6 +34,22 @@ async def click(selector: Annotated[str, "The properly formed query selector str
         raise ValueError('No active page found. OpenURL command opens a new page.')
 
     await browser_manager.highlight_element(selector, True)
+    
+    dom_changes_detected=None
+    def detect_dom_changes(changes:str): # type: ignore
+        nonlocal dom_changes_detected
+        dom_changes_detected = changes # type: ignore
+
+    subscribe(detect_dom_changes)
+    result = await do_click(page, selector, wait_before_execution)
+    await asyncio.sleep(0.1) # sleep for 100ms to allow the mutation observer to detect changes
+    unsubscribe(detect_dom_changes)
+    await browser_manager.notify_user(result["summary_message"])
+    if dom_changes_detected:
+        return f"{result['detailed_message']}.\n As a consequence of this action, new elements have appeared in view: {dom_changes_detected}. Get all_fields to interact with the elements."
+    return result["detailed_message"]
+
+
     result = await do_click(page, selector, wait_before_execution)
     await browser_manager.notify_user(result["summary_message"])
     return result["detailed_message"]
