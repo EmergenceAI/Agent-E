e19e268db3159e2812ade9b1c7e849ce68daa6c1
RuhanaAzam <azam.ruhana@gmail.com>
2024-06-24 09:20:28 -0500

    Fixed validator prompting to gpt4

--- ae/core/skills/do_self_validation.py
+++ ae/core/skills/do_self_validation.py
@@ -1,7 +1,9 @@
 from typing import Annotated
 from ae.utils.logger import logger
 
-import openai
+import os
+from openai import OpenAI
+from ae.core.prompts import LLM_PROMPTS
 
 async def do_self_validation(
         lastest_plan: Annotated[str, "The lasted version of the plan to verify."],
@@ -18,13 +20,19 @@ async def do_self_validation(
     logger.info("Proceeding with self-validation...\n")
 
     # Validation process
-    #prompt = f"Given a the webtask task {intent}, do you believe {lastest_plan} is sufficient in completing the afformentioned task? If not please provide feedback."
-    #openai.api_key = os.getenv("OPENAI_API_KEY")
-    #response = openai.Completion.create(model="gpt-4", prompt=prompt, max_tokens=100)
-    #print(response)
-    #exit()
-    
-    #feedback = "Your plan sucks. Please try again."
-    feedback = "Your plan is valid."
+    user_prompt = f"Given a the webtask task {intent}, do you believe {lastest_plan} is sufficient in completing the afformentioned task? Please answer yes or no. If the plan is not valid, please provide feedback on which portions are not valid and why."
+    OpenAI.api_key = os.getenv("OPENAI_API_KEY")
+    model_name = os.getenv("AUTOGEN_MODEL_NAME")
+    client = OpenAI()
+    feedback = client.chat.completions.create(
+        model=model_name,
+          messages=[
+            {"role": "system", "content": LLM_PROMPTS["VERFICATION_AGENT"]},
+            {"role": "user", "content": user_prompt},
+        ]
+    )
+    #feedback = feedback.choices[0].message.content #Extract message from chat completion object
+    # feedback = "Your plan sucks. Please try again."
+    #feedback = "Your plan is valid."
     print(f"*** Feedback is: {feedback}\n")
     return feedback
