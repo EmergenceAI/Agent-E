c2ca27b232dce8387d8e41a7861b5defe417007e
teaxio <tea@xioresearch.com>
2024-07-02 16:25:49 -0400

    use logger

--- ae/utils/js_helper.py
+++ ae/utils/js_helper.py
@@ -1,6 +1,8 @@
 import json
 import re
 
+from ae.utils.logger import logger
+
 
 def escape_js_message(message: str) -> str:
     """
@@ -25,8 +27,8 @@ def beautify_plan_message(message:str) -> str:
     Returns:
         str: The plan message with newlines added between each numbered step.
     """
-    print("beautify_plan_message original: "+message)
+    logger.debug(f"beautify_plan_message original:\n{message}")
     # Add a newline before each numbered step that is not already preceded by a newline
     plan_with_newlines = re.sub(r'(?<!\n)( \d+\.)', r'\n\1', message)
-    print("beautify_plan_message modified: "+plan_with_newlines)
+    logger.debug(f"beautify_plan_message modified:\n{plan_with_newlines}")
     return plan_with_newlines
